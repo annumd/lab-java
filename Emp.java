3b6ecfd5910e2a264591e9acce52d5f696b3258d
annumd <161940207+annumd@users.noreply.github.com>
2024-08-23 11:03:34 +0530

    Create Emp.java

---
+++ Emp.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Emp
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner (System.in)
+		int sal,hour;
+		public void funtion1(s,h)
+		{	
+			System.out.println("Enter Sallary: ");
+			sal=sc.nextInt();
+			System.out.println("Enter number of work hours: ");
+			hour=sc.nextInt();
+		}
+		public void addSal()
+		{
+			function1 f=new function1();
+			if(f.sal< 500)
+			{
+				sal+=10
