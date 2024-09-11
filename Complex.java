7aece6b20e723e0aa1ddc48da0e5603e48aa3c67
annumd <161940207+annumd@users.noreply.github.com>
2024-09-11 15:26:11 +0530

    Update Complex.java

--- Complex.java
+++ Complex.java
@@ -1,69 +1,68 @@
+//Complex.java
 import java.util.*;
+class Complex 
+{
+    	int real, img;
+	//method for addition
+    	static void Add(Complex c1, Complex c2) 
+    	{
+        	int res_real = c1.real + c2.real;
+        	int res_img = c1.img + c2.img;
+        	System.out.println(c1.real + "+" + c1.img + "i + " + c2.real + "+" + c2.img + "i = " + res_real + "+" + res_img + "i");
+    	}
+	//method for subtraction
+    	static void Sub(Complex c1, Complex c2) 
+    	{
+        		int res_real = c1.real - c2.real;
+        		int res_img = c1.img - c2.img;
+        		System.out.println(c1.real + "+" + c1.img + "i - " + c2.real + "+" + c2.img + "i = " + res_real + "+" + res_img + "i");
+    	}
+	//method for product
+    	static void Pro(Complex c1, Complex c2) 
+    	{
+        		int res_real = (c1.real * c2.real) - (c1.img * c2.img);
+        		int res_img = (c1.real * c2.img) + (c1.img * c2.real);
+        		System.out.println(c1.real + "+" + c1.img + "i * " + c2.real + "+" + c2.img + "i = " + res_real + "+" + res_img + "i");
+    	}
 
-class Complex {
-    int real, img;
+    	public static void main(String[] args) 
+    	{
+        		Complex one = new Complex();
+        		Complex two = new Complex();
+        		Scanner sc = new Scanner(System.in);
+		//read complex 1
+        		System.out.print("Enter Real part of Number One: ");
+        		one.real = sc.nextInt();
+        		System.out.print("Enter Imaginary part of Number One: ");
+        		one.img = sc.nextInt();
+		//read complex 2
+        		System.out.print("Enter Real part of Number Two: ");
+        		two.real = sc.nextInt();
+        		System.out.print("Enter Imaginary part of Number Two: ");
+        		two.img = sc.nextInt();
 
-    static void Add(Complex c1, Complex c2) {
-        int res_real = c1.real + c2.real;
-        int res_img = c1.img + c2.img;
-        System.out.println(c1.real + "+" + c1.img + "i + " + c2.real + "+" + c2.img + "i = " + res_real + "+" + res_img + "i");
-    }
+        		System.out.print("Addition of One and Two: ");
+        		Add(one, two);
 
-    static void Sub(Complex c1, Complex c2) {
-        int res_real = c1.real - c2.real;
-        int res_img = c1.img - c2.img;
-        System.out.println(c1.real + "+" + c1.img + "i - " + c2.real + "+" + c2.img + "i = " + res_real + "+" + res_img + "i");
-    }
+        		System.out.print("Subtraction of One and Two: ");
+        		Sub(one, two);
 
-    static void Pro(Complex c1, Complex c2) {
-        int res_real = (c1.real * c2.real) - (c1.img * c2.img);
-        int res_img = (c1.real * c2.img) + (c1.img * c2.real);
-        System.out.println(c1.real + "+" + c1.img + "i * " + c2.real + "+" + c2.img + "i = " + res_real + "+" + res_img + "i");
-    }
-
-    public static void main(String[] args) {
-        Complex one = new Complex();
-        Complex two = new Complex();
-        Scanner sd = new Scanner(System.in);
-
-        System.out.println("Enter Real part of Number One:");
-        one.real = sd.nextInt();
-        System.out.println("Enter Imaginary part of Number One:");
-        one.img = sd.nextInt();
-
-        System.out.println("Enter Real part of Number Two:");
-        two.real = sd.nextInt();
-        System.out.println("Enter Imaginary part of Number Two:");
-        two.img = sd.nextInt();
-
-        System.out.println("Addition of One and Two:");
-        Add(one, two);
-
-        System.out.println("Subtraction of One and Two:");
-        Sub(one, two);
-
-        System.out.println("Product of One and Two:");
-        Pro(one, two);
-
-        sd.close();
+        		System.out.print("Product of One and Two: ");
+        		Pro(one, two);
+        		
+      
     }
 }
 
+
 OUTPUT
 
-  ubuntu@l6sys24:~/SCS1051123$ javac Complex.java
+    ubuntu@l6sys24:~/SCS1051123$ javac Complex.java
 ubuntu@l6sys24:~/SCS1051123$ java Complex
-Enter Real part of Number One:
-3
-Enter Imaginary part of Number One:
-4
-Enter Real part of Number Two:
-1
-Enter Imaginary part of Number Two:
-2
-Addition of One and Two:
-3+4i + 1+2i = 4+6i
-Subtraction of One and Two:
-3+4i - 1+2i = 2+2i
-Product of One and Two:
-3+4i * 1+2i = -5+10i
+Enter Real part of Number One: 4
+Enter Imaginary part of Number One: 5
+Enter Real part of Number Two: 6
+Enter Imaginary part of Number Two: 7
+Addition of One and Two: 4+5i + 6+7i = 10+12i
+Subtraction of One and Two: 4+5i - 6+7i = -2+-2i
+Product of One and Two: 4+5i * 6+7i = -11+58i
