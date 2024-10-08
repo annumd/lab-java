09ea699324589a1b8c490b33d61e1dd1406566a8
annumd <161940207+annumd@users.noreply.github.com>
2024-10-08 21:12:55 +0530

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,213 +1,106 @@
 import java.util.*;
-
 class Q
-
  {
-
     int max,front,rear;
-
     int[] q;
-
     Q()
-
      {
-
          this.max=5;
-
          this.q=new int[max];
-
          this.front=-1;
-
          this.rear=-1;
-
      }
-
          Q(int max)
-
           {
-
             this.max=max;
-
             this.q=new int[max];
-
             this.front=-1;
-
             this.rear=-1;
-
- 
-
           }
 
 void enqueue(int item)
-
       {
-
-            if(rear==max-1)
-
+        if(rear==max-1)
          {
-
             System.out.println("Overflow");
-
          }
-
-            else
-
+        else
          {
-
             if(front==-1 && rear==-1)
-
               {
-
                 front=0;
-
               }
-
             rear++;
-
             q[rear]=item;
-
          }
-
-            display();
-
+        display();
       }
 
 void dequeue()
-
      {
-
             if(front==-1)
-
           {
-
             System.out.print("Underflow \n");
-
           }
-
             else
-
          {
-
             System.out.print(q[front++]+" popped \n");
-
          }
-
             display();
-
         }
-
-            void display()
-
-            {
-
-                    if(front==-1 && rear==-1 || front>rear)
-
+void display()
+{
+                 if(front==-1 && rear==-1 || front>rear)
                     {
-
                         System.out.print("Queue is Empty \n");
-
                     }
-
-                    else
-
-                    {
-
+                else
+                {
                      for(int i=front;i<=rear;i++)
-
                      {
-
                          System.out.print(q[i]+" ");
-
                      }
-
- 
-
                         System.out.println();
-
-                    }
-
-            }
-
+                }
+}
  }     
 
- 
-
- 
-
 public class Queue
-
  {
-
     public static void main(String[] args)
-
      {
-
         Scanner sc=new Scanner(System.in);
-
         Q queue=new Q();
-
         int ch,item,n;
-
         do
-
          {
-
             System.out.print("1.Enqueue 2.Dequeue 3.Display 4.Exit \n");
-
             System.out.print("Enter Choice: ");
-
             ch=sc.nextInt();
-
             switch(ch)
-
              {
-
                     case 1:
-
-                    System.out.print("Enter number of items: ");
-
-                    n=sc.nextInt();
-
-                    for(int i=0;i<n;i++)
-
-                    {
-
-                        System.out.print("Enter item: ");
-
-                        item=sc.nextInt();
-
-                        queue.enqueue(item);
-
-                    }
-
-                    break;
-
+	                    System.out.print("Enter number of items: ");
+	                    n=sc.nextInt();
+	                    for(int i=0;i<n;i++)
+	                    {
+	                        System.out.print("Enter item: ");
+	                        item=sc.nextInt();
+	                        queue.enqueue(item);
+	                    }
+	                    break;
                     case 2:
-
-                    queue.dequeue();
-
-                    break;
-
+	                    queue.dequeue();
+	                    break;
                     case 3:
-
-                    queue.display();
-
-                    break;
-
+	                    queue.display();
+	                    break;
                     case 4:
-
-                    break;
-
- 
-
+	                    break;
            }
-
         }
-
      while(ch!=4);
-
     }
-
  }
 
  
